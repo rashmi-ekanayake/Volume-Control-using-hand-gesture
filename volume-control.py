1b1b4b3d35f5e7ae880445ccdeb49f296823a705
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-05-16 07:53:19 +0530

    added hand tracking and draw landmarks.py

--- volume-control.py
+++ volume-control.py
@@ -3,12 +3,22 @@ import mediapipe as mp
 import pyautogui
 
 webcam = cv2.VideoCapture(0)
+my_hands = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
 
 
 while True:
 
     _ , image = webcam.read()
+    rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    output = my_hands.process(rgb_image)
+    hands = output.multi_hand_landmarks
+    if hands:
+        for hand in hands:  
+            drawing_utils.draw_landmarks(image,hand)  
+
     cv2.imshow("Hand Volume Control Using Python", image)
+    
     key = cv2.waitKey(10)
     if key == 27:
         break
