52f876b5f179b1add25716cc913c3413aac53339
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-05-16 08:49:48 +0530

    add volume control based on finger distance.py

--- volume-control.py
+++ volume-control.py
@@ -7,10 +7,10 @@ webcam = cv2.VideoCapture(0)
 my_hands = mp.solutions.hands.Hands()
 drawing_utils = mp.solutions.drawing_utils
 
-
 while True:
 
     _ , image = webcam.read()
+    image = cv2.flip(image, 1)
     frame_height , frame_width, _ = image.shape
     rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output = my_hands.process(rgb_image)
@@ -30,10 +30,16 @@ while True:
                     cv2.circle(img = image, center=(x,y) ,radius=8,color=(0,0,255), thickness=3)
                     x2 = x  
                     y2 = y
-    cv2.line(image,(x1,y1),(x2,y2),(0,255,0),5)
-    
+        dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2)** (0.5)//4
+        cv2.line(image,(x1,y1),(x2,y2),(0,255,0),5)
+        if dist > 50 :
+                pyautogui.press("volumeup")
+        else:
+                pyautogui.press("volumedown")
+
+
     cv2.imshow("Hand Volume Control Using Python", image)
-    
+
     key = cv2.waitKey(10)
     if key == 27:
         break
