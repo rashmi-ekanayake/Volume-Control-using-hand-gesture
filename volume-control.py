df2fa07783ef62fa41f271811eb9cb6bb4bbc7e1
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-05-16 08:37:20 +0530

    got fingertip coordinates.py

--- volume-control.py
+++ volume-control.py
@@ -10,13 +10,27 @@ drawing_utils = mp.solutions.drawing_utils
 while True:
 
     _ , image = webcam.read()
+    frame_height , frame_width, _ = image.shape
     rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     output = my_hands.process(rgb_image)
     hands = output.multi_hand_landmarks
     if hands:
         for hand in hands:  
             drawing_utils.draw_landmarks(image,hand)  
-
+            landmarks = hand.landmark
+            for id, landmarks in enumerate(landmarks):
+                x = int(landmarks.x * frame_width )
+                y = int(landmarks.y * frame_height )
+                if id == 8:
+                    cv2.circle(img = image, center=(x,y), radius=8,color=(0,255,255), thickness=3)
+                   
+                if id == 4:
+                    cv2.circle(img = image, center=(x,y) ,radius=8,color=(0,0,255), thickness=3)
+    
+    
+    
+    
+    
     cv2.imshow("Hand Volume Control Using Python", image)
     
     key = cv2.waitKey(10)
