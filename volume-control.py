504b65e07ceebd8a8d25eaf68c402ef2cde00422
RashmiAyodhya <jennie20.23rak@gmail.com>
2025-05-16 07:45:38 +0530

    setup webcam feed window for hand gesture input.py

---
+++ volume-control.py
@@ -0,0 +1,16 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+webcam = cv2.VideoCapture(0)
+
+
+while True:
+
+    _ , image = webcam.read()
+    cv2.imshow("Hand Volume Control Using Python", image)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
